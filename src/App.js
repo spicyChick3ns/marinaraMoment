cc023fc29e7d4b0271b8a1c6e650b6a53642f1c7
spicyChick3ns <gonzalesmsd@gmail.com>
2018-04-29 12:23:38 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Jumbotron} from 'react-bootstrap';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Jumbotron>
+        <p>hi</p>
+      </Jumbotron>
+
+    );
+  }
+}
+
+export default App;
